3465b38d29e42fa038e0a7cdf15476eb4e8bcb79
msgloriayang <yanggloria91@gmail.com>
2023-05-16 23:18:25 -0500

    added logic to database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,8 +14,38 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+try {
+  const db = await dbPromise;
+  const tx = db.transaction('content', 'readwrite');
+  const store = tx.objectStore('content');
+  await store.put(content);
+  await tx.done;
+  console.log('Good job! Content added to the database:', content);
+} catch (error) {
+  console.error('Content failed to add to the database:', error);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+try {
+  const db = await initdb();
+  if (!db) {
+    console.error('Database is not initialized.');
+    return [];
+  }
+  
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = await store.getAll();
+  await tx.done;
+  console.log('All content:', allContent);
+  return allContent;
+} catch (error) {
+  console.error('Failed to get content:', error);
+  return [];
+};
 
-initdb();
+// (async () => {
+// await initdb();
+// })();
+// initdb();
